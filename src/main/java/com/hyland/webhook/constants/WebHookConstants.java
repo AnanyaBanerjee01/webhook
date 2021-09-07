807a06aa34862f200ca14744b0dd2b2133e5de7d
Ananya Banerjee <Ananya.Banerjee@Hyland.com>
2021-09-07 18:25:54 +0530

    Adding logic for             1. Validating if the request originated from Zoom             2. Validate if the event notification is for Recording Completed event subscribed             3. Configuring the contants in a Constant file

---
+++ src/main/java/com/hyland/webhook/constants/WebHookConstants.java
@@ -0,0 +1,18 @@
+package com.hyland.webhook.constants;
+
+public class WebHookConstants {
+
+    private WebHookConstants() {
+    }
+
+    public static final String RECORDING_COMPLETED = "recording.completed";
+    public static final String HOST = "host";
+    public static final String CONTENT_TYPE = "content-type";
+    public static final String CONTENT_LENGTH = "content-length";
+    public static final String TRACKINGID = "x-zm-trackingid";
+    public static final String AUTHORIZATION = "authorization";
+    public static final String USER_AGENT = "user-agent";
+    public static final String CLIENTID = "clientid";
+    public static final String TRACEID = "x-amazon-trace-id";
+
+}
--- src/main/java/com/hyland/webhook/controller/WebhookController.java
+++ src/main/java/com/hyland/webhook/controller/WebhookController.java
@@ -2,63 +2,58 @@ package com.hyland.webhook.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyland.webhook.DTO.gen.RecordingCompletedSchema;
+import com.hyland.webhook.constants.WebHookConstants;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.hyland.webhook.DTO.gen.Object;
+
+@RestController
+@RequestMapping("/api/webhook")
+@Log4j2
+public class WebhookController {
+
+    @Value("${zoom.verification.token}")
+    private String verificationToken;
+
+    @RequestMapping("/get")
+    public String getNotification() {
+        log.info("#### Hello Webhooks ##### ");
+        return "Hello Webhooks!!!";
+    }
 
 
-    @RestController
-    @RequestMapping("/api/webhook")
-    @Log4j2
-    public class WebhookController {
+    @PostMapping
+    public ResponseEntity<String> onRecordingCompleted(@RequestBody String requestBody, @RequestHeader(value = WebHookConstants.HOST) String host, @RequestHeader(value = WebHookConstants.USER_AGENT) String userAgent, @RequestHeader(value = WebHookConstants.AUTHORIZATION) String authToken) {
+        log.info("#### Incoming Webhook Notification from Zoom API ##### {}", requestBody);
+        ObjectMapper mapper = new ObjectMapper();
+        log.debug("#### Request Header Information #####  Host :: User Agent :: {}   {} ", host, userAgent);
 
-        @RequestMapping ("/get")
-        public String getNotification() {
-            log.info("#### Hello Webhooks ##### ");
-            return "Hello Webhooks!!!";
-        }
-        @PostMapping
-        public ResponseEntity<String> consumeNotification(@RequestBody String requestBody) {
-            log.info("#### Incoming Webhook Notification from Zoom API ##### {}" , requestBody);
-            ObjectMapper mapper = new ObjectMapper();
-           try{
-               //Convert JSON object to Java POJO
-               Object recordingObject = mapper.readValue(requestBody, Object.class);
-               log.info("Recording Completed Event Payload {} ::", recordingObject);
-           }
-           catch(Exception e){
-                log.error("Exception occured while parsing the notification event {}",e);
+        try {
+            //Convert JSON object to Java POJO
+            RecordingCompletedSchema recordingObject = mapper.readValue(requestBody, RecordingCompletedSchema.class);
+            log.debug("Converted Json Payload to Object");
+
+            // verify if the event notification originated from Zoom
+            if (null != authToken && !authToken.equals(verificationToken)) {
+                log.debug("Invalid Verification Token");
+                return new ResponseEntity<>(requestBody, HttpStatus.FORBIDDEN);
+            }
+            // Validate if the event notification is for Recording Completed event subscribed
+            if (recordingObject.getEvent().equals(WebHookConstants.RECORDING_COMPLETED)) {
+                log.debug("Recording Completed Event Payload {} ::", recordingObject);
+
             }
 
-            return new ResponseEntity<>(requestBody, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Exception occured while parsing the notification event {}", e);
         }
 
-        /**
-         * Generate JWT token for Zoom using api credentials
-         *
-         * @return JWT Token String
-         */
-/*        private String generateZoomJWTTOken() {
-            String id = UUID.randomUUID().toString().replace("-", "");
-            SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-            Date creation = new Date(System.currentTimeMillis());
-            Date tokenExpiry = new Date(System.currentTimeMillis() + (1000 * 60));
-
-            Key key = Keys
-                    .hmacShaKeyFor(zoomApiSecret.getBytes());
-            return Jwts.builder()
-                    .setId(id)
-                    .setIssuer(zoomApiKey)
-                    .setIssuedAt(creation)
-                    .setSubject("")
-                    .setExpiration(tokenExpiry)
-                    .signWith(key, signatureAlgorithm)
-                    .compact();
-        }*/
-
-
+        return new ResponseEntity<>(requestBody, HttpStatus.OK);
     }
+
+
+}
 
